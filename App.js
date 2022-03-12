3cab5f56b1b825095e1637a76757c35c5b7a6867
jasonshamayev <jason.shamayev@mavs.uta.edu>
2022-03-12 13:32:37 -0600

    app updated

--- App.js
+++ App.js
@@ -1,21 +1,119 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, Button, SafeAreaView} from 'react-native';
+//import { Component } from 'react/cjs/react.production.min';
+//import quickstart from "./quickstart"
+//import submitToGoogle from "./vision"
+import config from './config';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends React.Component {
+  
+    state = {
+      loading: false,
+    };
+    
+    render() {
+      console.log('Starting app')
+      console.log('config', config);
+
+      return (
+        <View style={styles.container}>
+          <TouchableOpacity
+          onPress={() => this.submitToGoogle()}
+          style={styles.button}>
+            <Text>Click</Text>
+          </TouchableOpacity>
+        </View>
+      );
+    }
+    submitToGoogle = async () => {
+      try {
+        this.setState({ uploading: true });
+        let { image } = this.state;
+        let body = JSON.stringify({
+          requests: [
+            {
+              features: [
+                //{ type: 'LABEL_DETECTION', maxResults: 10 },
+                //{ type: 'LANDMARK_DETECTION', maxResults: 5 },
+                //{ type: 'FACE_DETECTION', maxResults: 5 },
+                //{ type: 'LOGO_DETECTION', maxResults: 5 },
+                { type: 'TEXT_DETECTION', maxResults: 5 },
+                //{ type: 'DOCUMENT_TEXT_DETECTION', maxResults: 5 },
+                //{ type: 'SAFE_SEARCH_DETECTION', maxResults: 5 },
+                //{ type: 'IMAGE_PROPERTIES', maxResults: 5 },
+                //{ type: 'CROP_HINTS', maxResults: 5 },
+                //{ type: 'WEB_DETECTION', maxResults: 5 }
+              ],
+              image: {
+                source: {
+                  imageUri: "gs://audibraille-vision/Kushal.jpg"
+                }
+              }
+            }
+          ]
+        });
+        let response = await fetch(
+          'https://vision.googleapis.com/v1/images:annotate?key=' +
+            '',
+          {
+            headers: {
+              Accept: 'application/json',
+              'Content-Type': 'application/json'
+            },
+            method: 'POST',
+            body: body
+          }
+        );
+        let responseJson = await response.json();
+        const {responses} = responseJson
+        
+        console.log(responses.fullTextAnnotation);
+        //console.log(responseJson);
+        
+        //console.log(responseJson);
+
+        //responseJson.parse("text");
+        //console.log(responseJson)
+        this.setState({
+          googleResponse: responseJson,
+          uploading: false
+        });
+      } catch (error) {
+        console.log(error);
+      }
+    };
 }
+// function App() {
+//   const buttonClickedHandler = () => {
+//     console.log('button clicked');
+//     //need to have camera take picture here
+//   };
+  
+  
+//     return (
+//           <View style={styles.container}>
+//             <TouchableOpacity 
+//               onPress={() => this.submitToGoogle('Kushal.jpg')}
+//               style = {styles.button}
+//               >
+//                 <Text> Click </Text>
+//               </TouchableOpacity>
+//           </View>      
+//     );
+//   } 
+
+ //export default App;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#f01d71',
+    justifyContent: "center",
+    alignItems: "center",
   },
-});
+  button: {
+    height: 300,
+    marginTop: 200,
+  },
+})
+
