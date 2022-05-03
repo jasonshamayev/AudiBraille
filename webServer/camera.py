687d45ea352581a47b119e7186c0e1dce796f1bf
jasonshamayev <jason.shamayev@mavs.uta.edu>
2022-05-03 18:01:36 -0500

    server updated to autograb ip

--- webServer/camera.py
+++ webServer/camera.py
@@ -3,6 +3,7 @@ from time import sleep
 
 camera = PiCamera()
 camera.resolution = (1080,720)
+camera.rotation = 270
 camera.start_preview()
 sleep(5)
 camera.capture('image.jpg')
--- webServer/server.py
+++ webServer/server.py
@@ -1,7 +1,22 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import time
+import socket
 
-hostName = "10.0.0.162"
+def get_ip():
+    s=socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.settimeout(0)
+    try:
+        s.connect(('10.255.255.255',1))
+        IP = s.getsockname()[0]
+    except Exception:
+        IP = '127.0.0.1'
+    finally:
+        s.close()
+    return IP
+
+ip = get_ip()
+
+hostName = ip
 serverPort = 8080
 
 class MyServer(BaseHTTPRequestHandler):
